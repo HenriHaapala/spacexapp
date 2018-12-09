4588cd5ab980fffe31f116f0f28b5768b75f65b6
Henri Haapala <henri.haapala@valamis.com>
2018-12-09 23:00:52 +0200

    Space x launches app

---
+++ public/precache-manifest.5430f68e8d5d0e9ee274cd6461b34db2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdeef645d16fa156cd30",
+    "url": "/static/js/main.cdeef645.chunk.js"
+  },
+  {
+    "revision": "3fadb6410cf5793da420",
+    "url": "/static/js/1.3fadb641.chunk.js"
+  },
+  {
+    "revision": "cdeef645d16fa156cd30",
+    "url": "/static/css/main.57cbc042.chunk.css"
+  },
+  {
+    "revision": "7621ec6f61cc38791f851179f1228d2f",
+    "url": "/index.html"
+  }
+];
